19bde2c15d7793c19c462abea653f218a05e4b3e
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-17 21:23:06 +0600

    Learn about useNavigate

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
  
 const Post = ({post}) => {
     const {id, title} = post;
+    const navigate = useNavigate()
+    const handlePost = ()=>{
+        navigate(`/post/${id}`)
+    }
     return (
         <div>
             <h2><Link to={`/post/${id}`}>Post: {title}</Link></h2>
+            <button onClick={handlePost}>Show Post</button>
         </div>
     );
 };
