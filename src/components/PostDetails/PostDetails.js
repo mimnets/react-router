1e85273a8c2c3e1a39d728657e60339b96c5598e
Monirul Islam <mdmonirulislammonir@gmail.com>
2022-11-17 23:13:07 +0600

    navigate

--- src/components/PostDetails/PostDetails.js
+++ src/components/PostDetails/PostDetails.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const PostDetails = () => {
     const postDetails = useLoaderData()
+    const navigate = useNavigate()
+    const {title, body, userId} = postDetails;
+    const handleAuthor = () =>{
+        navigate(`/friend/${userId}`)
+    }
     // console.log(postDetails)
     return (
         <div>
-            <h3>About Post Details : {postDetails.title}</h3>
-            <p>{postDetails.body}</p>
+            <h3>About Post Details : {title}</h3>
+            <p>{body}</p>
+            <button onClick={handleAuthor}>Show the Author</button>
         </div>
     );
 };
